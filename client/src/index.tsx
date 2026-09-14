92138afcc777b2ee93f0cc7c35d5e30fb7b22a01
warwar91 <113282781@qq.com>
2026-09-14 20:51:42 +0800

    Restore full application code, remove debug tests

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './app';
 import './index.css';
 
-// 最简单的测试：直接渲染一个div
-const SimpleTest = () => {
-  return (
-    <div style={{ padding: '40px', fontFamily: 'sans-serif', background: 'lightblue' }}>
-      <h1>React最简单测试</h1>
-      <p>如果你能看到这个页面，说明React正常工作。</p>
-    </div>
-  );
-};
-
-ReactDOM.createRoot(document.getElementById('root')!).render(<SimpleTest />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
